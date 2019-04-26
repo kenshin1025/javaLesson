94456ea38826168e0ef915ef7a12038bce5a0b6b
x18067xx <x18067xx@aitech.ac.jp>
2019-04-27 00:21:49 +0900

    課題3ー2にテストコード追加

--- 03/UtilityMethods.java
+++ 03/UtilityMethods.java
@@ -8,13 +8,19 @@ public class UtilityMethods {
         app.printCurrency(0);
         app.printCurrency(-1);
         app.printCurrency(1.155);
+        app.printCurrency(100);
 
         System.out.println(app.add(1, 2));
         System.out.println(app.add(-1, 2));
         System.out.println(app.add(0, 0));
         System.out.println(app.add(999999999, 1));
-        app.printCurrency(100);
 
+
+        System.out.println(division(2, 2));
+        System.out.println(division(0, 2));
+        System.out.println(division(2, 0));
+        System.out.println(division(2, 1));
+        System.out.println(division(2, 400));
 
         System.out.println(app.getTheAnswerToTheUltimateQuestionOfLife_TheUniverse_And_Everything());
     }
